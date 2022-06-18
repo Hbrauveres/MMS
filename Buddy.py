d819e11c9905bc2d83d542f2e1fecf3586bdf258
Tiago Villa <tiago_villa@hotmail.com>
2022-06-18 17:35:19 -0300

    Adição parcial do algoritmo buddy

---
+++ Buddy.py
@@ -0,0 +1,53 @@
+# cria a memória inicial (preenche a memória com 0)
+def cria_memoria(tam_memoria):
+    return [0 for i in range(tam_memoria)]
+
+
+# devolve uma lista com duas listas
+def particiona_memoria(memoria):
+    return (memoria[i::2] for i in range(2))
+
+# retorna verdadeiro ou falso se o bloco de memória está desocupado (todo preenchido com 0)
+
+
+def checa_memoria_vazia(memoria):
+    return memoria.count(memoria[0] == len(memoria))
+
+
+def insere_na_memoria(memoria, id_processo, tamanho_processo):
+
+    # ATENÇÃO: aqui deve conferir se é cada bloco de memória ou toda a memória
+    if len(memoria) >= tamanho_processo and len(memoria) > 0:
+        memoria_particionada = particiona_memoria(memoria)
+
+        for memoria in memoria_particionada:
+            # checa se memoria é vazia (ou seja, todos os elementos = 0)
+            if(checa_memoria_vazia(memoria)):
+                # se sim, preenche o bloco de memoria
+                for i in range(0, tamanho_processo):
+                    memoria[i] = id_processo
+
+    return memoria
+
+
+def run_buddy(tam_memoria, fila_entrada):
+    # preenche memória
+    memoria = cria_memoria(tam_memoria)
+
+    for i in fila_entrada:
+        # Insere ou remove da memoria
+        if i[0] == "IN":
+            tam_processo = int(i[2])
+            nome_processo = i[1]
+
+            memoria = insere_na_memoria(memoria, nome_processo, tam_processo)
+            pass
+        elif i[0] == "OUT":
+            pass
+
+
+memoria = [0 for i in range(4)]
+
+memoria_particionada = particiona_memoria(memoria, 3)
+for mem in memoria_particionada:
+    print(mem)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os 
+import os
 import ParticaoFixa as PF
 import AlocacaoDinamica as AD
+import Buddy as BD
+
 
 def potencia_de_dois(n):
     if n == 0:
@@ -11,8 +13,10 @@ def potencia_de_dois(n):
         n = n / 2
     return True
 
+
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def tamanho_particao_valido(tam_particao, tam_memoria):
     if tam_memoria % tam_particao == 0:
@@ -20,51 +24,57 @@ def tamanho_particao_valido(tam_particao, tam_memoria):
     else:
         return False
 
-def main(): 
+
+def main():
 
     while(1):
-        #clear_terminal()
+        # clear_terminal()
         # Cria fila de entradas
         fila_de_entrada = []
 
         # Pergunta o nome do arquivo
-        #clear_terminal()
+        # clear_terminal()
         nome_arquivo = input("Informe o nome do arquivo:\n> ")
 
         # Abre o arquivo e carrega na fila de entrada
-        #clear_terminal()
+        # clear_terminal()
         with open(nome_arquivo, "r") as arquivo:
             for linha in arquivo:
-                fila_de_entrada.append(linha.replace(",","").replace("("," ").replace(")", "").split())
+                fila_de_entrada.append(linha.replace(",", "").replace(
+                    "(", " ").replace(")", "").split())
                 print(fila_de_entrada)
 
         # Pergunta o tipo de particionamento
         tipo_particionamento = -1
         while(tipo_particionamento != 1 and tipo_particionamento != 2 and tipo_particionamento != 3):
-            #clear_terminal()
-            tipo_particionamento = int(input("Informe o tipo de particionamento:\n[1] Particao Fixa\n[2] Particao Variavel\n[3] Particao Buddy\n> "))
-         
+            # clear_terminal()
+            tipo_particionamento = int(input(
+                "Informe o tipo de particionamento:\n[1] Particao Fixa\n[2] Particao Variavel\n[3] Particao Buddy\n> "))
+
         # Pergunta o tamanho da memoria
         tamanho_memoria = -1
         while(1):
-            #clear_terminal()
+            # clear_terminal()
             tamanho_memoria = int(input("Informe o tamanho da memoria:\n> "))
             if potencia_de_dois(tamanho_memoria) == False:
-                input("Tamanho da memoria deve ser potencia de dois!\nPressione enter para continuar...")
+                input(
+                    "Tamanho da memoria deve ser potencia de dois!\nPressione enter para continuar...")
             else:
-                #clear_terminal()
+                # clear_terminal()
                 break
 
         politica = -1
         while(1):
-            #clear_terminal()
-            politica = int(input("Informe a politica:\n>[1] Best Fit\n[2] Worst Fit\n> "))
+            # clear_terminal()
+            politica = int(
+                input("Informe a politica:\n>[1] Best Fit\n[2] Worst Fit\n> "))
             if politica != 1 and politica != 2:
-                input("Tamanho da memoria deve ser potencia de dois!\nPressione enter para continuar...")
+                input(
+                    "Tamanho da memoria deve ser potencia de dois!\nPressione enter para continuar...")
             else:
-                #clear_terminal()
+                # clear_terminal()
                 break
-        
+
         tamanho_particao = -1
         while(1):
             tamanho_particao = input("Informe o tamanho da particao:\n> ")
@@ -72,24 +82,28 @@ def main():
                 tamanho_particao = int(tamanho_particao)
                 break
             else:
-                input("Tamanho de partição invalido!\n\nPressione enter para continuar...")
-
+                input(
+                    "Tamanho de partição invalido!\n\nPressione enter para continuar...")
 
         # Chama o tipo de particionamento
         if tipo_particionamento == 1:
-            PF.run_particao_fixa(tamanho_memoria, fila_de_entrada, tamanho_particao)
+            PF.run_particao_fixa(
+                tamanho_memoria, fila_de_entrada, tamanho_particao)
         elif tipo_particionamento == 2:
-            AD.run_alocacao_dinamica(tamanho_memoria, fila_de_entrada, politica)
+            AD.run_alocacao_dinamica(
+                tamanho_memoria, fila_de_entrada, politica)
         elif tipo_particionamento == 3:
-            pass
-        
+            BD.run_buddy(tamanho_memoria, fila_de_entrada)
+
         restart_flag = -1
         while(restart_flag != 0 and restart_flag != 1):
-            restart_flag = int(input("Deseja reiniciar o programa?\n[1] Sim\n[0] Não\n> "))
-            #clear_terminal()
-        
+            restart_flag = int(
+                input("Deseja reiniciar o programa?\n[1] Sim\n[0] Não\n> "))
+            # clear_terminal()
+
         if restart_flag == 0:
-           exit()
-        
+            exit()
+
+
 if __name__ == "__main__":
     main()
